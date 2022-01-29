c8fa84198dafb3cb88c47306e6be389c6a35422e
mahafuztechie <mahafuzaadeez5@gmail.com>
2022-01-29 13:36:21 +0530

    [mahafuz] adding uc3 create Queue

---
+++ StacksandQueues/StacksandQueues/LinkedListQueue.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StacksandQueues
+{
+    internal class LinkedListQueue
+    {
+        //variable
+        private Node front;
+        /// Enqueues the specified value.
+        public void Enqueue(int value)
+        {
+            Node node = new Node(value);
+            if (this.front == null)
+                this.front = node;
+            else
+            {
+                Node rear = front;
+                while (rear.next != null)
+                {
+                    rear = rear.next;
+                }
+                rear.next = node;
+            }
+            Console.WriteLine("{0} Enqueued into Queue", value);
+        }
+        /// display the elements of Queue.
+        public void Display()
+        {
+            if (front == null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            else
+            {
+                Node temp = front;
+                while (temp != null)
+                {
+                    Console.WriteLine("Element in Queue:" + temp.data);
+                    temp = temp.next;
+                }
+            }
+        }
+    }
+}
--- StacksandQueues/StacksandQueues/Program.cs
+++ StacksandQueues/StacksandQueues/Program.cs
@@ -21,11 +21,12 @@ namespace StacksandQueues
         public static void CallStackQueue()
         {
             LinkedListStack linkedListStack = new LinkedListStack();
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
             bool CONTINUE = true;
             while (CONTINUE)
             {
                 Console.WriteLine("\nEnter your option!");
-                Console.WriteLine("1=Stack , 2=Exit");
+                Console.WriteLine("1=Stack , 2=queue , 3=Exit");
                 int choice = int.Parse(Console.ReadLine());
                 switch (choice)
                 {
@@ -46,6 +47,13 @@ namespace StacksandQueues
                         linkedListStack.Display();
                         break;
                     case 2:
+                        Console.WriteLine("<---Creating A Queue---> \n");
+                        linkedListQueue.Enqueue(56);
+                        linkedListQueue.Enqueue(30);
+                        linkedListQueue.Enqueue(70);
+                        linkedListQueue.Display();
+                        break;
+                    case 3:
                         CONTINUE = false;
                         break;
                     default:
